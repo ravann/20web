658e6c0b338d945ec7f8cc6366e3a5f205b32d6b
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2020-04-16 07:37:09 +0800

    Movie booking is now fully functioning ...

--- 2_movie_booking/script.js
+++ 2_movie_booking/script.js
@@ -5,17 +5,57 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const tickerPrice = +movieSelect.value;
+var tickerPrice = +movieSelect.value;
+
+// Call init functions ...
+populateUI();
+
+// Set intial count and value
+updateSelectedCount();
+
+// Get data from local storage and populate UI
+function populateUI() {
+  ss = localStorage.getItem("selectedSeats");
+  const selectedSeats = JSON.parse(ss);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const movie = JSON.parse(localStorage.getItem("selectedMovie"));
+  movieSelect.selectedIndex = movie;
+
+  updateSelectedCount();
+}
 
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const allSeats = [...seats];
+
+  const seatsIndex = [...selectedSeats].map((i) => allSeats.indexOf(i));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  tickerPrice = +movieSelect.value;
   sts = selectedSeats.length;
   ttl = sts * tickerPrice;
   count.innerText = sts;
   total.innerText = ttl;
 }
 
+// Movie change event
+movieSelect.addEventListener("change", (e) => {
+  tickerPrice = +e.target.value;
+  console.log(JSON.stringify(e.target.selectedIndex));
+  localStorage.setItem("selectedMovie", JSON.stringify(e.target.selectedIndex));
+  updateSelectedCount();
+});
+
+// Seat click evwnt
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
