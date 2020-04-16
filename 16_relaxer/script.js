9415bff7e63552ac54eb6ea383965fd19cdcb3cb
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2020-04-16 23:39:24 +0800

    Completed relaxer scripts ...

---
+++ 16_relaxer/script.js
@@ -0,0 +1,27 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+console.log(container);
+
+breatheAnimation(true);
+
+function breatheAnimation(first) {
+  text.innerText = "Breathe In";
+  container.className = "container grow";
+
+  setTimeout(() => {
+    text.innerText = "Hold";
+    setTimeout(() => {
+      text.innerText = "Breathe Out";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(() => {
+  breatheAnimation();
+}, totalTime);
